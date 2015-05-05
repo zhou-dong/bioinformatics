07355f6bf45cb966438c2111b34890c19cb0a6d4
zhou-dong <82224165@qq.com>
2015-05-05 12:47:49 -0500

    update svm 2 kmeans

---
+++ k-means.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+
--- svm.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use("ggplot")
-from sklearn import svm
-
